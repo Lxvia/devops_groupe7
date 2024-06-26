f4695e43dadb9cd068ee2dde4a2cc8d9bb956ad2
Lxvia <oliviarudolf67@gmail.com>
2024-06-26 14:26:32 +0200

    commit olivia

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './styles/index.scss';
 import Accueil from './pages/Accueil.js';
 import Navbar from './composants/Navbar.js';
+import Resultat from "./pages/Resultat.js";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Accueil />} />
+          <Route path="/resultat" element={<Resultat />} />
         </Routes>
       </BrowserRouter>
     </div>
